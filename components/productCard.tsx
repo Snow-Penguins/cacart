f33a738055d32d4728c6f553f785fc68dacc7ab8
Euna Kim <eunak21@gmail.com>
2024-03-31 15:06:28 -0600

    Run prettier-fix script

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import styles from '../styles/productCard.module.css'
+import React from "react";
+import styles from "../styles/productCard.module.css";
 
-interface Product{
-    id: number;
-    category: string;
-    name: string;
-    price: number;
+interface Product {
+  id: number;
+  category: string;
+  name: string;
+  price: number;
 }
 
 interface ProductCardProps {
-    product: Product; 
-  }
+  product: Product;
+}
 
-const ProductCard: React.FC<ProductCardProps>= ({product}) => {
-
-    const productImages = ['/images/product1.jpg', '/images/product2.jpg', '/images/product3.jpg'];
-    const productImage = productImages[(product.id - 1) % productImages.length];
-    return (
-        <div className="border border-gray-200 rounded-md w-[450px] h-[450px] mb-10">
-            <div className={styles['image-container']} >
-                <img src={productImage} alt="Product Image" />
-            </div>
-            <div className = "mt-6 ml-6">
-                <h6 className="text-secondary_text text-sm mb-5">{product.category}</h6>
-                <h2 className="font-semibold text-black text-lg mb-2">{product.name}</h2>
-                <h6 className="font-semibold text-black text-sm mb-2">${product.price.toFixed(2)}</h6>
-            </div>
-        </div>
-    )
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const productImages = [
+    "/images/product1.jpg",
+    "/images/product2.jpg",
+    "/images/product3.jpg",
+  ];
+  const productImage = productImages[(product.id - 1) % productImages.length];
+  return (
+    <div className="border border-gray-200 rounded-md w-[450px] h-[450px] mb-10">
+      <div className={styles["image-container"]}>
+        <img src={productImage} alt="Product Image" />
+      </div>
+      <div className="mt-6 ml-6">
+        <h6 className="text-secondary_text text-sm mb-5">{product.category}</h6>
+        <h2 className="font-semibold text-black text-lg mb-2">
+          {product.name}
+        </h2>
+        <h6 className="font-semibold text-black text-sm mb-2">
+          ${product.price.toFixed(2)}
+        </h6>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- components/productCardList.tsx
+++ components/productCardList.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import ProductCard from './productCard';
+import React from "react";
+import ProductCard from "./productCard";
 
 interface Product {
-    id: number;
-    category: string;
-    name: string;
-    price: number;
+  id: number;
+  category: string;
+  name: string;
+  price: number;
 }
 
 interface ProductCardListProps {
-    products: Product[];
+  products: Product[];
 }
 
 const ProductCardList: React.FC<ProductCardListProps> = ({ products }) => {
-    return (
-        <div className="flex flex-wrap justify-center gap-4 pt-10">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-                
-            ))}
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap justify-center gap-4 pt-10">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductCardList;
-
