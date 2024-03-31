4f12551d23af8bb43f60e99b2740b2da787108b4
Euna Kim <eunak21@gmail.com>
2024-03-31 13:45:08 -0600

    Create product list component and UI

---
+++ components/productCardList.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ProductCard from './productCard';
+
+interface Product {
+    id: number;
+    category: string;
+    name: string;
+    price: number;
+}
+
+interface ProductCardListProps {
+    products: Product[];
+}
+
+const ProductCardList: React.FC<ProductCardListProps> = ({ products }) => {
+    return (
+        <div className="flex flex-wrap justify-center gap-4 pt-10">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+                
+            ))}
+        </div>
+    );
+};
+
+export default ProductCardList;
+
