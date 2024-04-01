57207c58d2deb30f74af034bd6eb65a7b5930644
HarryLee87 <haewon.harry.lee@gmail.com>
2024-04-01 14:12:22 -0600

    Fixed navigation bar design same as figma

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -10,17 +10,28 @@ export default function Navigation() {
       </div>
 
       <div className="flex flex-1 justify-center gap-4">
-        <button className="px-4 h-12">Categories</button>
-        <div className="flex w-3/4 max-w-xl border border-gray-300 rounded-3xl overflow-hidden">
+        <div className="flex items-center">
+          <div className="flex flex-col justify-center items-center mr-2">
+            <span className="block w-3 min-h-0.5 bg-black" />
+            <span className="block w-3 min-h-0.5 bg-black my-0.5" />
+            <span className="block w-3 min-h-0.5 bg-black" />
+          </div>
+          <button className="mr-3 h-12 text-sm">Categories</button>
+        </div>
+        <form className="flex items-center w-3/4 max-w-xl rounded-full overflow-hidden bg-gray-200 border-2 border-black focus-within:border-blue-500">
           <input
             type="search"
-            className="w-full px-4 h-12 py-2 text-sm text-gray-700 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            className="flex-grow px-4 h-12 text-sm text-gray-700 bg-gray-200 rounded-full outline-none"
             placeholder="Search Your Item"
             required
           />
           <button
             type="submit"
-            className="px-4 h-12 py-2 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-r-md"
+            className="flex items-center justify-center mr-1.5 text-white bg-primary rounded-full"
+            style={{
+              width: "2.25rem",
+              height: "80%",
+            }}
           >
             <svg
               className="w-5 h-5"
@@ -34,7 +45,7 @@ export default function Navigation() {
               <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
             </svg>
           </button>
-        </div>
+        </form>
       </div>
 
       <div className="flex gap-2 mr-4">
