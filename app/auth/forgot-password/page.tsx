f577309ed5c6d5778d2c9f4e398d6fd68d33ebcd
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-04-08 00:36:50 -0600

    Delete h1 tag of forgot password in ForgotPassword Page

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,10 +1,5 @@
 import ForgotPassword from "@/components/ForgotPassword";
 
 export default function Page() {
-  return (
-    <div>
-      <h1>Forgot Password Page</h1>
-      <ForgotPassword />;
-    </div>
-  );
+  return <ForgotPassword />;
 }
