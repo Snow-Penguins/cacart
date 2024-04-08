51001b27363a05c7bc6fcfb886d2efc4d213908c
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-04-08 00:34:22 -0600

    Create forgot password page, linked it from signin page

---
+++ app/auth/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import ForgotPassword from "@/components/ForgotPassword";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Forgot Password Page</h1>
+      <ForgotPassword />;
+    </div>
+  );
+}
