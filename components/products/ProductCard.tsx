253033d080ff27d330ddc304581b5292edc249ec
Euna Kim <eunak21@gmail.com>
2024-04-13 23:26:09 -0600

    try to fix file name

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Product } from "./Product";
+import { Product } from "../../entities/Product";
 import Link from "next/link";
 
 const PRODUCT_IMAGES = [
--- entities/Product.ts
+++ entities/Product.ts
@@ -4,3 +4,7 @@ export interface Product {
   name: string;
   price: number;
 }
+
+export interface ProductList {
+  products: Product[];
+}
