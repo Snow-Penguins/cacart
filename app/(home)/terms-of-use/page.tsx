3976ae15083a56eaac557dd0f390257a5314c2f2
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-05-02 18:57:49 -0600

    initial commit-create terms-of-use directory and page, link from footer

---
+++ app/(home)/terms-of-use/page.tsx
@@ -0,0 +1,52 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-h1">Terms of Use</h1>
+      <h6>
+        Welcome to Cacart. We’re delighted to have you here. Please make
+        yourself comfortable and enjoy your time, while ensuring to abide by our
+        house rules.
+      </h6>
+      <p>
+        This policy took effect in May 2024. Please note that Section 11 of the
+        Terms below contains a binding arbitration agreement and class action
+        waiver for users in Canada. By agreeing to the Terms, you and Cacart
+        agree to submit any disputes between us exclusively to individual
+        arbitration and not to sue in court, except in the limited circumstances
+        described below.
+      </p>
+      <p>
+        1. Accepting These Terms
+        <br />
+        2. Those Other Documents We Mentioned
+        <br />
+        3. Your Privacy
+        <br />
+        4. Your Account with Cacart
+        <br />
+        5. Your Content
+        <br />
+        6. Your Use of Our Services
+        <br />
+        7. Termination
+        <br />
+        8. Warranties and Limitation of Liability (or the Things You Can’t Sue
+        Us For)
+        <br />
+        9. Indemnification (and What Happens If You Get Us Sued)
+        <br />
+        10. Disputes with Other Users
+        <br />
+        11. Arbitration and Dispute Resolution Agreement (Canada)
+        <br />
+        12. Dispute Resolution (Rest of World)
+        <br />
+        13. Changes to the Terms
+        <br />
+        14. Some Finer Legal Points
+        <br />
+        15. Contact Information.
+      </p>
+    </div>
+  );
+}
