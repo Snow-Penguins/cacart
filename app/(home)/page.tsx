14ca034d3729f1dd6083bafce493e1ffd07c0ab0
JIYEON-H <jiyeon.heo0906@gmail.com>
2024-05-11 10:18:30 -0600

    delete 'Test' text

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-h1">Welcome to CaCart Ecommerce Platform!!</h1>
-      <a>Test</a>
       <ProductCardList />
     </main>
   );
