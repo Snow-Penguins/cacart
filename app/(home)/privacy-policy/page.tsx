6fda99f2b1ccd4a725f728b45e4f719cc9255ba9
Sarah Jeong <sarahjeong16@gmail.com>
2024-05-15 17:19:38 -0600

    add cotents to privacy policy page

---
+++ app/(home)/privacy-policy/page.tsx
@@ -0,0 +1,81 @@
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <div className="mb-4">
+        <div className="text-h1 mb-2">Privacy Policy</div>
+        <div className="text-lg">This policy took effect on 15 May 2024.</div>
+      </div>
+      <br />
+
+      <div className="space-y-4">
+        <div>
+          <span className="font-bold text-lg">1. Scope of Policy</span>
+          <p>
+            This Privacy Policy applies to CaCart, our mobile applications
+            (CaCart App), CaCart Payments, and other related services. It covers
+            how we manage personal information for registered users, guests, and
+            other users of our services.
+          </p>
+        </div>
+        <div>
+          <span className="font-bold text-lg">
+            2. Information Collection and Use
+          </span>
+          <p>
+            We collect personal information to provide and improve our services,
+            maintain security, and comply with legal obligations. This includes
+            buyer and seller information, device data, and usage details.
+          </p>
+        </div>
+        <div>
+          <span className="font-bold text-lg">3. Sharing of Information</span>
+          <p>
+            Information may be shared with CaCart group companies, third-party
+            service providers, and as publicly displayed on profiles and
+            listings. It may also be shared with other parties like business
+            partners and legal authorities when necessary.
+          </p>
+        </div>
+        <div>
+          <span className="font-bold text-lg">
+            4. Data Security and Retention
+          </span>
+          <p>
+            We employ security measures to protect personal information and
+            retain it as long as necessary for service provision, compliance,
+            and legal obligations.
+          </p>
+        </div>
+        <div>
+          <span className="font-bold text-lg">5. User Rights and Choices</span>
+          <p>
+            Users have rights to access, edit, or delete their personal
+            information, subject to legal restrictions. Options to manage
+            consent and marketing preferences are available through account
+            settings.
+          </p>
+        </div>
+        <div>
+          <span className="font-bold text-lg">
+            6. Changes to Privacy Policy
+          </span>
+          <p>
+            Updates to the policy will be communicated through our services and,
+            if significant, via email.
+          </p>
+        </div>
+        <div>
+          <span className="font-bold text-lg">7. Contact Information</span>
+          <p>
+            For questions or to exercise privacy rights, contact CaCart&#39;s
+            Support team via the Help Centre or at{" "}
+            <a href="mailto:support@cacart.com" className="underline">
+              support@cacart.com
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
