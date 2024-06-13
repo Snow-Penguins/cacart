6a688dcf076551915aa6294d5882e8437b41dd08
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-06-13 16:26:24 -0600

    Fix error in google sign-in

--- hooks/auth/useGoogleLogin.ts
+++ hooks/auth/useGoogleLogin.ts
@@ -1,8 +1,10 @@
 export const useGoogleLogin = () => {
   const Backend_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  const handleLogin = () => {
-    window.location.href = `${Backend_URL}/auth/google`;
+  const handleLogin = async () => {
+    const response = await fetch(`${Backend_URL}/auth/google`);
+    const data = await response.json();
+    window.location.href = data.url;
   };
 
   return handleLogin;
