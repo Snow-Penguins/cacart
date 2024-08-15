5d722df06f52c037e8f16e11d2ef436e191b9b1d
Sarah Jeong <sarahjeong16@gmail.com>
2024-08-14 22:41:05 -0600

    style address fields

--- components/checkout/ShippingAddress.tsx
+++ components/checkout/ShippingAddress.tsx
@@ -82,7 +82,9 @@ const ShippingAddress: React.FC<ShippingAddressProps> = ({
     }
   };
 
-  const handleNewAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleNewAddressChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
     const { name, value } = e.target;
     setNewAddress((prev) => {
       const updated = { ...prev, [name]: value };
@@ -138,58 +140,103 @@ const ShippingAddress: React.FC<ShippingAddressProps> = ({
 
       {selectedAddressType === "new" && (
         <div className="mt-4 space-y-4">
-          <input
-            type="text"
-            name="unit_number"
-            placeholder="Unit Number"
-            onChange={handleNewAddressChange}
-            value={newAddress.unit_number || ""}
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="address_line1"
-            placeholder="Address Line 1"
-            onChange={handleNewAddressChange}
-            value={newAddress.address_line1 || ""}
-            required
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="address_line2"
-            placeholder="Address Line 2"
-            onChange={handleNewAddressChange}
-            value={newAddress.address_line2 || ""}
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="city"
-            placeholder="City"
-            onChange={handleNewAddressChange}
-            value={newAddress.city || ""}
-            required
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="province"
-            placeholder="Province"
-            onChange={handleNewAddressChange}
-            value={newAddress.province || ""}
-            required
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="postal_code"
-            placeholder="Postal Code"
-            onChange={handleNewAddressChange}
-            value={newAddress.postal_code || ""}
-            required
-            className="w-full p-2 border border-gray-300 rounded"
-          />
+          <div>
+            <label htmlFor="unit_number">Unit Number</label>
+            <input
+              type="text"
+              name="unit_number"
+              id="unit_number"
+              placeholder="101"
+              onChange={handleNewAddressChange}
+              value={newAddress.unit_number || ""}
+              maxLength={25}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+
+            <label htmlFor="address_line1">Address Line 1</label>
+            <input
+              type="text"
+              name="address_line1"
+              id="address_line1"
+              placeholder="789 Pitfield Blvd"
+              onChange={handleNewAddressChange}
+              value={newAddress.address_line1 || ""}
+              maxLength={50}
+              required
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+
+            <label htmlFor="address_line2">Address Line 2</label>
+            <input
+              type="text"
+              name="address_line2"
+              id="address_line2"
+              placeholder="Apt 85"
+              onChange={handleNewAddressChange}
+              value={newAddress.address_line2 || ""}
+              maxLength={50}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+
+            <div className="mt-1 flex space-x-4">
+              <div className="flex-1">
+                <label htmlFor="city">City</label>
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  placeholder="Calgary"
+                  onChange={handleNewAddressChange}
+                  value={newAddress.city || ""}
+                  maxLength={25}
+                  required
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
+              </div>
+              <div className="flex-1">
+                <label htmlFor="province">Province</label>
+                <select
+                  name="province"
+                  id="province"
+                  onChange={handleNewAddressChange}
+                  value={newAddress.province || ""}
+                  required
+                  className="w-full p-2 border border-gray-300 rounded"
+                >
+                  <option value="" disabled>
+                    Select a Province
+                  </option>
+                  <option value="AB">Alberta</option>
+                  <option value="BC">British Columbia</option>
+                  <option value="MB">Manitoba</option>
+                  <option value="NB">New Brunswick</option>
+                  <option value="NL">Newfoundland and Labrador</option>
+                  <option value="NS">Nova Scotia</option>
+                  <option value="ON">Ontario</option>
+                  <option value="PE">Prince Edward Island</option>
+                  <option value="QC">Quebec</option>
+                  <option value="SK">Saskatchewan</option>
+                  <option value="NT">Northwest Territories</option>
+                  <option value="NU">Nunavut</option>
+                  <option value="YT">Yukon</option>
+                </select>
+              </div>
+              <div className="flex-1">
+                <label htmlFor="postal_code">Postal Code</label>
+                <input
+                  type="text"
+                  name="postal_code"
+                  id="postal_code"
+                  placeholder="A2B9Q2"
+                  onChange={handleNewAddressChange}
+                  value={newAddress.postal_code || ""}
+                  maxLength={6}
+                  required
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       )}
 
